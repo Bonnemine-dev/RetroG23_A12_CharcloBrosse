2efc0a9c5e0a3fa47bffb74c5087972211dbc66b
Melvin Warnet <melvin.warnet@gmail.com>
2023-06-05 11:32:13 +0200

    Déclaration de la classe Tileset

--- CharcloBrosse/CharcloBrosse/utils/tileset.h
+++ CharcloBrosse/CharcloBrosse/utils/tileset.h
@@ -2,13 +2,31 @@
 #ifndef TILESET_H
 #define TILESET_H
 
+#include <QPixmap>
+#include <string>
+
+
 
 
 
 class TileSet
 {
+private:
+    std::string itsFilePath;
+    QPixmap itsPLayerTile;
+    QPixmap itsBlockTilesList[2][1];
+    QPixmap itsEnemyTilesList[2][1];
+    QPixmap itsSpawnerTilesList[2][1];
+    QPixmap itsDespawnerTilesList[2][1];
 public:
-    TileSet();
+    TileSet(std::string aFilePath);
+    QPixmap getItsPlayerTile();
+    QPixmap getItsBlockTile();
+    QPixmap getItsBlockHitTile();
+    QPixmap getItsEnemyTile();
+    QPixmap getItsEnemyHitTile();
+    QPixmap getItsSpawnerTile(int index);
+    QPixmap getItsDespawnerTile(int index);
 };
 
 #endif // TILESET_H
