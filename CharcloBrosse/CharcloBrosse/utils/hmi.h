d6f99b5063f90fd8a38b5dbf8d4de1bdf5630ea7
erwan godelle <erwan.godelle@etudiant.univ-lr.fr>
2023-06-05 11:41:05 +0200

    ajout de la déclaration de HMI

--- CharcloBrosse/CharcloBrosse/utils/hmi.h
+++ CharcloBrosse/CharcloBrosse/utils/hmi.h
@@ -1,8 +1,24 @@
-
 #ifndef HMI_H
 #define HMI_H
 
 #include <QWidget>
+#include <QKeyEvent>
+#include <string>
+#include <vector>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include "game.h"
+
+enum MenuState {
+    MAINMENU,
+    PAUSEMENU,
+    GAMEOVER,
+    GAME
+};
+
 
 
 
@@ -10,10 +26,23 @@ class HMI : public QWidget
 
 {
     Q_OBJECT
-
+private:
+    Game *itsGame;
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
+    QVBoxLayout *mainLayout;
+    QListWidget *highscoreList;
+    QPushButton *startGameButton;
+    QPushButton *rulesButton;
+    QPushButton *quitGameButton;
 public:
-    HMI(QWidget *parent = nullptr);
+    HMI(Game *itsGame, QWidget *parent = nullptr);
     ~HMI();
+private slots:
+    void displayMainMenu(std::vector <std::pair <std::string , unsigned int >> highscores);
+    void displayPauseMenu();
+    void displayGameOverMenu(std::vector <std::pair <std::string , unsigned int >> highscores);
+    void refreshAll();
 };
 
 #endif // HMI_H
