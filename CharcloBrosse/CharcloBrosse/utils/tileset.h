f26be742f6f00a8fc48c074fa0d3d7654599e27c
Melvin Warnet <melvin.warnet@gmail.com>
2023-06-05 11:51:31 +0200

    Correction retour de pointeurs

--- CharcloBrosse/CharcloBrosse/utils/tileset.h
+++ CharcloBrosse/CharcloBrosse/utils/tileset.h
@@ -13,20 +13,20 @@ class TileSet
 {
 private:
     std::string itsFilePath;
-    QPixmap itsPLayerTile;
-    QPixmap itsBlockTilesList[2][1];
-    QPixmap itsEnemyTilesList[2][1];
-    QPixmap itsSpawnerTilesList[2][1];
-    QPixmap itsDespawnerTilesList[2][1];
+    QPixmap* itsPLayerTile;
+    QPixmap* itsBlockTilesList[2][1];
+    QPixmap* itsEnemyTilesList[2][1];
+    QPixmap* itsSpawnerTilesList[2][1];
+    QPixmap* itsDespawnerTilesList[2][1];
 public:
     TileSet(std::string aFilePath);
-    QPixmap getItsPlayerTile();
-    QPixmap getItsBlockTile();
-    QPixmap getItsBlockHitTile();
-    QPixmap getItsEnemyTile();
-    QPixmap getItsEnemyHitTile();
-    QPixmap getItsSpawnerTile(int index);
-    QPixmap getItsDespawnerTile(int index);
+    QPixmap* getItsPlayerTile();
+    QPixmap* getItsBlockTile();
+    QPixmap* getItsBlockHitTile();
+    QPixmap* getItsEnemyTile();
+    QPixmap* getItsEnemyHitTile();
+    QPixmap* getItsSpawnerTile(int index);
+    QPixmap* getItsDespawnerTile(int index);
 };
 
 #endif // TILESET_H
