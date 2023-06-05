04094a6c658864fc67e718c6c393f99efff2fadf
tom bonneau <tom.bonneau@etudiant.univ-lr.fr>
2023-06-05 10:39:24 +0200

    Création de la classe abstraite Entity

--- CharcloBrosse/CharcloBrosse/Entity/entity.cpp
+++ CharcloBrosse/CharcloBrosse/Entity/entity.cpp
@@ -1,6 +1,26 @@
 
 #include "entity.h"
 
+unsigned short Entity::getItsX() const
+{
+    return itsX;
+}
+
+unsigned short Entity::getItsY() const
+{
+    return itsY;
+}
+
+unsigned short Entity::getItsHeight() const
+{
+    return itsHeight;
+}
+
+unsigned short Entity::getItsWidth() const
+{
+    return itsWidth;
+}
+
 Entity::Entity()
 {
 
--- CharcloBrosse/CharcloBrosse/Entity/entity.h
+++ CharcloBrosse/CharcloBrosse/Entity/entity.h
@@ -1,14 +1,22 @@
-
 #ifndef ENTITY_H
 #define ENTITY_H
 
-
-
+#include "utils/sprite.h"
 
 class Entity
 {
+protected:
+    unsigned short itsX;
+    unsigned short itsY;
+    unsigned short itsHeight;
+    unsigned short itsWidth;
+    Sprite * itsSprite;
 public:
     Entity();
+    unsigned short getItsX() const;
+    unsigned short getItsY() const;
+    unsigned short getItsHeight() const;
+    unsigned short getItsWidth() const;
 };
 
 #endif // ENTITY_H
