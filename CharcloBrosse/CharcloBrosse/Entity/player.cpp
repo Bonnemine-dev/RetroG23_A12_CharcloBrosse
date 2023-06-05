b449bd94d22b21129b4eb4960d70a34ef7219460
tom bonneau <tom.bonneau@etudiant.univ-lr.fr>
2023-06-05 15:33:46 +0200

    Modif Player et Enemy

--- CharcloBrosse/CharcloBrosse/Entity/player.cpp
+++ CharcloBrosse/CharcloBrosse/Entity/player.cpp
@@ -1,37 +1,43 @@
 
 #include "player.h"
 
-bool Player::getItsState() const
-{
-    return itsState;
-}
+//bool Player::getItsState() const
+//{
+//    return itsState;
+//}
 
+//Methode move
 void Player::move()
 {
     itsRect.setX(itsRect.x() + itsXSpeed);
     itsRect.setY(itsRect.y() + itsYSpeed);
 }
 
+//Setter itsXSpeed
 void Player::setItsXSpeed(short newItsXSpeed)
 {
     itsXSpeed = newItsXSpeed;
 }
 
+//Setter itsYSpeed
 void Player::setItsYSpeed(short newItsYSpeed)
 {
     itsYSpeed = newItsYSpeed;
 }
 
+//Setter itsLivesNb
 void Player::setItsLivesNb(unsigned short newItsLivesNb)
 {
     itsLivesNb = newItsLivesNb;
 }
 
+//Getter itsLivesNb
 unsigned short Player::getItsLivesNb() const
 {
     return itsLivesNb;
 }
 
+//Constructeur
 Player::Player(int x, int y, int height, int width, Sprite* sprite)
     : Entity(x, y, height, width, sprite)
 {
