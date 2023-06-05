2d9c2b6aef8ab4807cfc510614344fff06ca28e9
Arthur ANCIEN <aancien@etudiant.univ-lr.fr>
2023-06-05 11:41:24 +0200

    déclaration Game.h

--- CharcloBrosse/CharcloBrosse/utils/game.h
+++ CharcloBrosse/CharcloBrosse/utils/game.h
@@ -2,15 +2,43 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+#include <Qrect>
+#include "Entity/player.h"
+#include "utils/hmi.h"
+#include "utils/tileset.h"
+#include "utils/level.h"
+
+//#include "DB_Score.h"
+
 /**
  * @brief The Game class
  */
 
 
-class Game
-{
+class Game {
+private:
+    TileSet* itsTileSet;
+    Player* itsPlayer;
+    unsigned int itsScore;
+    Level* itsLevel;
+    HMI* itsHMI;
+//    DB_Score itsDBScore;
+    /* Les collisions à check
+     * P to E
+     * P to P
+     * E to E
+     * E to B
+     * P to B
+     * E to S
+     * P to M
+     */
+    void checkAllCollid();
+    void isLevelFinished();
+    void moveAll();
 public:
-    Game();
+void gameLoop();
 };
+
 
 #endif // GAME_H
