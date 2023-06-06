1055ee5676e0f481e9fd788e568a8614b044a137
tom bonneau <tom.bonneau@etudiant.univ-lr.fr>
2023-06-06 14:42:21 +0200

    Modif typedef PLAYERMAXSPEED et STILL

--- CharcloBrosse/CharcloBrosse/typedef.h
+++ CharcloBrosse/CharcloBrosse/typedef.h
@@ -5,4 +5,7 @@
 #define WIDTH 40 // the width of the window in tile
 #define COUNTER_VALUE 150 // define value to restart block state
 
+#define PLAYERMAXSPEED 2 // define value of max X speed
+#define STILL 0 // define value of still
+
 #endif // TYPEDEF_H
