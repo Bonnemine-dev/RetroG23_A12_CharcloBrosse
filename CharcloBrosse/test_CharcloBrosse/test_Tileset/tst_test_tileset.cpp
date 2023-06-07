7d8cb41b74f4f8e2f15f77bb525725e1e34b942c
Arthur ANCIEN <aancien@etudiant.univ-lr.fr>
2023-06-07 15:52:55 +0200

    Ajouts d'un test pour le constructeur de la class Tileset. NE FONCTIONNE PAS LA CRÉATION DE QPIXMAP IMPOSSIBLE ENDEHORS DE L'APPLICATION.

--- CharcloBrosse/test_CharcloBrosse/test_Tileset/tst_test_tileset.cpp
+++ CharcloBrosse/test_CharcloBrosse/test_Tileset/tst_test_tileset.cpp
@@ -1,4 +1,6 @@
 #include <QtTest>
+#include <QGuiApplication>
+#include <QPixmap>
 #include "../../CharcloBrosse/tileset.h"
 // add necessary includes here
 
@@ -6,6 +8,8 @@ class test_Tileset : public QObject
 {
     Q_OBJECT
 private:
+    //    std::string itsTilesetFilePath = "../../../CharcloBrosse/CharcloBrosse/ressources/tileset0.png";
+    std::string itsTilesetFileUnreadablePath = "../../../CharcloBrosse/CharcloBrosse/ressources/notexist.png";
     TileSet* itsTileset;
 public:
     test_Tileset();
@@ -15,7 +19,8 @@ private slots:
     void init();
     void cleanup();
 
-    void test_constructor();
+    //    void test_Constructor_UnreadableFile_Exception();
+    void test_Constructor_Generals();
 };
 
 test_Tileset::test_Tileset()
@@ -30,19 +35,44 @@ test_Tileset::~test_Tileset()
 
 void test_Tileset::init()
 {
-    itsTileset = new TileSet("../../CharcloBrosse/ressources/tileset0.png");
+    try
+    {
+        itsTileset = new TileSet("../../CharcloBrosse/ressources/tileset_test.png");
+        delete itsTileset;
+    }
+    catch(const std::string exceptionMessage)
+    {
+        if(exceptionMessage == "File unfound (Tileset::Tileset(const std:string aFilePath)")
+        {
+            QFAIL("File not found");
+        }
+    }
 }
+
 void test_Tileset::cleanup()
 {
     delete itsTileset;
 }
 
+//void test_Tileset::test_Constructor_UnreadableFile_Exception()
+//{
+//    // Définir le chemin d'accès à un fichier inexistant
+//    std::string filePath = "chemin/vers/fichier/inexistant.png";
 
-void test_Tileset::test_constructor()
+//    // Vérifier si l'exception est lancée
+//    QVERIFY_EXCEPTION_THROWN(TileSet tileSet(filePath),std::string);
+//}
+
+void test_Tileset::test_Constructor_Generals()
 {
-
+    QPixmap* BLOCK_TEST = new QPixmap(32,32);
+    QColor* COLOR_BLOCK_TEST = new QColor(255,0,0,255);
+    BLOCK_TEST->fill(*COLOR_BLOCK_TEST);
+    delete COLOR_BLOCK_TEST;
+    QCOMPARE(itsTileset->getItsBlockTile(),BLOCK_TEST);
+    delete BLOCK_TEST;
 }
 
-QTEST_APPLESS_MAIN(test_Tileset)
+QTEST_MAIN(test_Tileset)
 
 #include "tst_test_tileset.moc"
