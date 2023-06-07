90c36007b87d59dcef23ef590eb763688340fbca
erwan godelle <erwan.godelle@etudiant.univ-lr.fr>
2023-06-07 16:45:15 +0200

    tests sprite

--- CharcloBrosse/test_CharcloBrosse/test_Sprite/tst_test_sprite.cpp
+++ CharcloBrosse/test_CharcloBrosse/test_Sprite/tst_test_sprite.cpp
@@ -1,18 +1,28 @@
 #include <QtTest>
-
-// add necessary includes here
+#include <QPixmap>
+#include <QPainter>
+#include "../../CharcloBrosse/sprite.h"
 
 class test_Sprite : public QObject
 {
     Q_OBJECT
+
+    Sprite * sprite;
+    QPixmap * image;
 
 public:
     test_Sprite();
     ~test_Sprite();
 
 private slots:
-    void test_case1();
+    void initTestCase();
+    void cleanupTestCase();
+    void init();
+    void cleanup();
 
+    void test_constructor();
+    void test_moveTo();
+    // Display test would require a more complex setup and is not included here
 };
 
 test_Sprite::test_Sprite()
@@ -25,11 +35,39 @@ test_Sprite::~test_Sprite()
 
 }
 
-void test_Sprite::test_case1()
+void test_Sprite::initTestCase()
 {
-
+    image = new QPixmap();
 }
 
-QTEST_APPLESS_MAIN(test_Sprite)
+void test_Sprite::cleanupTestCase()
+{
+    delete image;
+}
+
+void test_Sprite::init()
+{
+    sprite = new Sprite(image);
+}
+
+void test_Sprite::cleanup()
+{
+    delete sprite;
+}
+
+void test_Sprite::test_constructor()
+{
+    QVERIFY(sprite != nullptr);
+}
+
+void test_Sprite::test_moveTo()
+{
+    sprite->moveTo(10, 20);
+    // Verify the new position. This assumes that you have getters for itsX and itsY
+    QCOMPARE(sprite->getItsX(), (unsigned short)10);
+    QCOMPARE(sprite->getItsY(), (unsigned short)20);
+}
+
+QTEST_MAIN(test_Sprite)
 
 #include "tst_test_sprite.moc"
