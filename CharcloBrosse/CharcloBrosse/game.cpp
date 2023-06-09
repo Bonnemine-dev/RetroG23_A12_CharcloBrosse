a1ce988ccfaf2feebb762fd493d4da0308b2ff76
tom bonneau <tom.bonneau@etudiant.univ-lr.fr>
2023-06-09 15:42:56 +0200

    Mofif crash

--- CharcloBrosse/CharcloBrosse/game.cpp
+++ CharcloBrosse/CharcloBrosse/game.cpp
@@ -20,9 +20,9 @@ void displayCoord(Entity * entity1, Entity * entity2){
 
 Game::Game()
 {
-    itsTileSet = new TileSet("/Users/bonnemine/Library/Mobile Documents/com~apple~CloudDocs/BUT_1 /S2/SAE2/SAE1256/build-CharcloBrosse-Qt_6_5_0_for_macOS-Debug/ressources/tilsetCharclo.png");
+    itsTileSet = new TileSet("C:/Users/tombo/Desktop/src/tilsetCharclo.png");
     itsPlayer = new Player((32*39)/2, 250, 64, 32, itsTileSet->getItsPlayerTile());
-    itsLevel = new Level("/Users/bonnemine/Library/Mobile Documents/com~apple~CloudDocs/BUT_1 /S2/SAE2/SAE1256/build-CharcloBrosse-Qt_6_5_0_for_macOS-Debug/ressources/oneenemy.json",itsTileSet);
+    itsLevel = new Level("C:/Users/tombo/Desktop/src/oneenemy.json",itsTileSet);
     itsHMI = new HMI(itsLevel, itsPlayer, this);
     itsEllapsedTime = 0;
     itsHMI->show();
@@ -44,11 +44,11 @@ void Game::gameLoop()
     itsEllapsedTime += 0.16;//0.16
 
 
-    if(!itsLevel->getItsEnemiesList().empty())std::cout<<"L'état du enemy : "<<itsLevel->getItsEnemiesList().at(itsLevel->getItsEnemiesList().size()-1)->getItsState()<<"\n";
+    if(!itsLevel->getItsEnemiesList().empty())//std::cout<<"L'état du enemy : "<<itsLevel->getItsEnemiesList().at(itsLevel->getItsEnemiesList().size()-1)->getItsState()<<"\n";
 
     if(!(itsLevel->getItsEnemyAppearsTimes().empty()) && itsEllapsedTime >= itsLevel->getItsEnemyAppearsTimes().at(0))
     {
-        std::cout<<"Je passe dans la condition pour faire apparaitre un enemy \n";
+        //std::cout<<"Je passe dans la condition pour faire apparaitre un enemy \n";
         Enemy * enemy = itsLevel->getItsRemainingEnemies().at(0);
         Sides side = itsLevel->getItsEnemyAppearsSides().at(0);
         switch(side){
@@ -63,21 +63,21 @@ void Game::gameLoop()
         itsEllapsedTime = 0;
     }
 
-    system("clear");
     checkAllCollid();
-    //    //std::cout<<"Est sur une platforme : "<<itsPlayer->getIsOnTheGround()<<"\n";
+    //    ////std::cout<<"Est sur une platforme : "<<itsPlayer->getIsOnTheGround()<<"\n";
     moveAll();
     itsHMI->refreshAll();
-    std::cout<<"count jump = "<<countJump<<"\n";
+    //std::cout<<"count jump = "<<countJump<<"\n";
     countJump = (countJump != 0?countJump = countJump - 1:countJump);
     //    qint64 elapsed = timer.nsecsElapsed(); // Temps écoulé en nanosecondes
-    //    //std::cout << "Temps écoulé:" << elapsed << "nanosecondes";
-    std::cout<<" taille de la list d'ennemies\n";
+    //    ////std::cout << "Temps écoulé:" << elapsed << "nanosecondes";
+    //std::cout<<" taille de la list d'ennemies\n";
     if((itsLevel->getItsEnemiesList().empty() && itsLevel->getItsRemainingEnemies().empty()) || itsPlayer->getItsLivesNb() == 0)
     {
-        std::cout<<"Je passe pas la condition de list vide\n";
+        //std::cout<<"Je passe pas la condition de list vide\n";
         itsHMI->stopGame();
     }
+    std::cout<<"sgdiysgc";
 }
 
 
@@ -97,7 +97,7 @@ void Game::checkAllCollid(){
     itsPlayer->setIsOnTheGround(false);
     // qWarning() << "player to block";
     for (Block * block : itsLevel->getItsBlockList()){
-        //        //std::cout<<"Le x de e2 : "<<itsPlayer->getItsX()<<" / le x + width de e1 : "<<block->getItsX() + block->getItsWidth()<<" / soit la condition : "<<(itsPlayer->getItsX() > (block->getItsX() + block->getItsWidth()))<<"\n";
+        //        ////std::cout<<"Le x de e2 : "<<itsPlayer->getItsX()<<" / le x + width de e1 : "<<block->getItsX() + block->getItsWidth()<<" / soit la condition : "<<(itsPlayer->getItsX() > (block->getItsX() + block->getItsWidth()))<<"\n";
         if(block->getItsCounter() != 0)//changement de la tuile quand elle est frappé
         {
             block->setItsCounter(block->getItsCounter() - 1);
@@ -144,10 +144,10 @@ void Game::checkAllCollid(){
 
         qWarning() << "player to enemy";
         for(Enemy * enemy : enemyList){
-            std::cout<<"#1";
+            //std::cout<<"#1";
             if(collid(itsPlayer, enemy)){
-                std::cout<<"#2";
-                std::cout<<"collision entre player et ennemy\n";
+                //std::cout<<"#2";
+                //std::cout<<"collision entre player et ennemy\n";
                 colBtwPlayerAndEnemy(itsPlayer, enemy);
             }
         }
@@ -173,7 +173,7 @@ void Game::checkAllCollid(){
                 }
             }
             if (enemyGravity){
-                std::cout<<"La gravité est appliqué\n";
+                //std::cout<<"La gravité est appliqué\n";
                 enemy->setItsYSpeed(GRAVITY);
             }
             else{
@@ -202,7 +202,7 @@ void Game::checkAllCollid(){
         for (Enemy * enemy : enemyList){
             for (Despawner * despawner : itsLevel->getItsDespawnerList()){
                 if(collid(enemy, despawner)){
-                    std::cout<<"je passe par la méthode colbtwenemyanddespawner\n";
+                    //std::cout<<"je passe par la méthode colbtwenemyanddespawner\n";
                         colBtwEnemyAndDespawner(enemy, despawner);
                 }
             }
@@ -212,9 +212,9 @@ void Game::checkAllCollid(){
 
 void Game::colBtwPlayerAndEnemy(Player* thePlayer,Enemy* theEnemy)
 {
-    std::cout<<"je suis dans la fonction qui crash\n";
-    std::cout<<"#3";
-    if(theEnemy->getItsState()&&std::cout<<"#4")//quand l'ennemie n'est PAS KO
+    //std::cout<<"je suis dans la fonction qui crash\n";
+    //std::cout<<"#3";
+    if(theEnemy->getItsState())//quand l'ennemie n'est PAS KO
     {
         thePlayer->setItsLivesNb(thePlayer->getItsLivesNb() - 1);
         thePlayer->setX((32*39)/2);
@@ -285,41 +285,41 @@ bool Game::isOnTop(Entity * entity, Block * block){
 
 bool Game::collid(Entity * entity1, Entity * entity2){
 
-    //std::cout<<"La height du player = "<<entity1->getItsHeight()<<"\n";
-    //std::cout<<"La width du player = "<<entity1->getItsWidth()<<"\n";
-    //std::cout<<"Le Y du player : "<<entity1->getItsY()<<"\n";
-    //std::cout<<"Le X du player : "<<entity1->getItsX()<<"\n";
+    ////std::cout<<"La height du player = "<<entity1->getItsHeight()<<"\n";
+    ////std::cout<<"La width du player = "<<entity1->getItsWidth()<<"\n";
+    ////std::cout<<"Le Y du player : "<<entity1->getItsY()<<"\n";
+    ////std::cout<<"Le X du player : "<<entity1->getItsX()<<"\n";
 
-    //std::cout<<"Est trop à droite : "<<(entity1->getItsX() > (entity2->getItsX() + entity2->getItsWidth()))<<"\n";
-    //std::cout<<"Car le coté d de pl : "<<entity1->getItsX()<<" est plus grand que le coté g de bl : "<<(entity2->getItsX() + entity2->getItsWidth())<<"\n";
+    ////std::cout<<"Est trop à droite : "<<(entity1->getItsX() > (entity2->getItsX() + entity2->getItsWidth()))<<"\n";
+    ////std::cout<<"Car le coté d de pl : "<<entity1->getItsX()<<" est plus grand que le coté g de bl : "<<(entity2->getItsX() + entity2->getItsWidth())<<"\n";
     if(entity1->getItsX() > (entity2->getItsX() + entity2->getItsWidth())){      // trop à droite
         // qWarning() << "trop à droite";
-        //std::cout<<"Trop à droite\n";
+        ////std::cout<<"Trop à droite\n";
         return false;
     }
-    //std::cout<<"Est trop à gauche : "<<((entity1->getItsX() + entity1->getItsWidth()) < entity2->getItsX())<<"\n";
+    ////std::cout<<"Est trop à gauche : "<<((entity1->getItsX() + entity1->getItsWidth()) < entity2->getItsX())<<"\n";
     if((entity1->getItsX() + entity1->getItsWidth()) < entity2->getItsX()){ // trop à gauche
         // qWarning() << "trop à gauche";
-        //std::cout<<"Trop à gauche\n";
+        ////std::cout<<"Trop à gauche\n";
         return false;
     }
-    //std::cout<<"Est trop en bas : "<<(entity1->getItsY() > (entity2->getItsY() + entity2->getItsHeight()))<<"\n";
+    ////std::cout<<"Est trop en bas : "<<(entity1->getItsY() > (entity2->getItsY() + entity2->getItsHeight()))<<"\n";
     if(entity1->getItsY() > (entity2->getItsY() + entity2->getItsHeight())){ // trop en bas
         // qWarning() << "trop en bas";
-        //std::cout<<"Trop en bas\n";
+        ////std::cout<<"Trop en bas\n";
         return false;
     }
 
-    //std::cout<<"Le Y + H de player : "<<(entity1->getItsY() + entity1->getItsHeight())<<" / le Y du block : "<<entity2->getItsY()<<" / soit la condition : "<<((entity1->getItsY() + entity1->getItsHeight()) > entity2->getItsY())<<"\n";
+    ////std::cout<<"Le Y + H de player : "<<(entity1->getItsY() + entity1->getItsHeight())<<" / le Y du block : "<<entity2->getItsY()<<" / soit la condition : "<<((entity1->getItsY() + entity1->getItsHeight()) > entity2->getItsY())<<"\n";
     if((entity1->getItsY() + entity1->getItsHeight()) < entity2->getItsY()){  // trop en haut
         // qWarning() << "trop en haut";
-        //        //std::cout<<"La height du player = "<<entity1->getItsHeight()<<"\n";
-        //        //std::cout<<"La width du player = "<<entity1->getItsWidth()<<"\n";
-        //        //std::cout<<"Le Y du player : "<<entity1->getItsY()<<"\n";
-        //std::cout<<"Trop en haut\n";
+        //        ////std::cout<<"La height du player = "<<entity1->getItsHeight()<<"\n";
+        //        ////std::cout<<"La width du player = "<<entity1->getItsWidth()<<"\n";
+        //        ////std::cout<<"Le Y du player : "<<entity1->getItsY()<<"\n";
+        ////std::cout<<"Trop en haut\n";
         return false;
     }
-    //std::cout<<"En collision\n";
+    ////std::cout<<"En collision\n";
     return true;
 }
 
@@ -341,12 +341,12 @@ void Game::onLeftKeyPressed()
 void Game::onRightKeyPressed()
 {
     itsPlayer->setItsXSpeed(PLAYERMAXSPEED);
-    //std::cout<<"appuie du right : "<<itsPlayer->getItsXSpeed()<<"\n";
+    ////std::cout<<"appuie du right : "<<itsPlayer->getItsXSpeed()<<"\n";
 }
 
 void Game::onUpKeyPressed()
 {
-    std::cout<<itsPlayer->getIsOnTheGround()<<" / est sur le sol? passe par upkey\n";
+    //std::cout<<itsPlayer->getIsOnTheGround()<<" / est sur le sol? passe par upkey\n";
     if(itsPlayer->getIsOnTheGround()){
         itsPlayer->setItsYSpeed(-PLAYERMAXSPEED);
         countJump = 6*32;
