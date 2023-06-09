065bb9709a884e9d9b4fa6ce980711386aefdadb
Arthur ANCIEN <aancien@etudiant.univ-lr.fr>
2023-06-09 16:22:49 +0200

    La c'est bon c'est sur

--- CharcloBrosse/CharcloBrosse/game.cpp
+++ CharcloBrosse/CharcloBrosse/game.cpp
@@ -20,9 +20,9 @@ void displayCoord(Entity * entity1, Entity * entity2){
 
 Game::Game()
 {
-    itsTileSet = new TileSet("../ressources/tilsetCharclo.png");
+    itsTileSet = new TileSet("/Users/bonnemine/Library/Mobile Documents/com~apple~CloudDocs/BUT_1 /S2/SAE2/SAE1256/build-CharcloBrosse-Qt_6_5_0_for_macOS-Debug/ressources/tilsetCharclo.png");
     itsPlayer = new Player((32*39)/2, 250, 64, 32, itsTileSet->getItsPlayerTile());
-    itsLevel = new Level("/Users/bonnemine/Library/Mobile Documents/com~apple~CloudDocs/BUT_1 /S2/SAE2/SAE1256/ressources/oneenemy.json",itsTileSet);
+    itsLevel = new Level("/Users/bonnemine/Library/Mobile Documents/com~apple~CloudDocs/BUT_1 /S2/SAE2/SAE1256/build-CharcloBrosse-Qt_6_5_0_for_macOS-Debug/ressources/oneenemy.json",itsTileSet);
     itsHMI = new HMI(itsLevel, itsPlayer, this);
     itsEllapsedTime = 0;
     itsHMI->show();
@@ -46,22 +46,22 @@ void Game::gameLoop()
 
     if(!itsLevel->getItsEnemiesList().empty())std::cout<<"L'état du enemy : "<<itsLevel->getItsEnemiesList().at(itsLevel->getItsEnemiesList().size()-1)->getItsState()<<"\n";
 
-            if(!(itsLevel->getItsEnemyAppearsTimes().empty()) && itsEllapsedTime >= itsLevel->getItsEnemyAppearsTimes().at(0))
-        {
-            std::cout<<"Je passe dans la condition pour faire apparaitre un enemy \n";
-            Enemy * enemy = itsLevel->getItsRemainingEnemies().at(0);
-            Sides side = itsLevel->getItsEnemyAppearsSides().at(0);
-            switch(side){
-            case LEFT:
-                itsLevel->getItsSpawnerList().at(0)->appears(enemy);
-                break;
-            case RIGHT:
-                itsLevel->getItsSpawnerList().at(1)->appears(enemy);
-                break;
-            }
-            itsLevel->appears(enemy);
-            itsEllapsedTime = 0;
+    if(!(itsLevel->getItsEnemyAppearsTimes().empty()) && itsEllapsedTime >= itsLevel->getItsEnemyAppearsTimes().at(0))
+    {
+        std::cout<<"Je passe dans la condition pour faire apparaitre un enemy \n";
+        Enemy * enemy = itsLevel->getItsRemainingEnemies().at(0);
+        Sides side = itsLevel->getItsEnemyAppearsSides().at(0);
+        switch(side){
+        case LEFT:
+            itsLevel->getItsSpawnerList().at(0)->appears(enemy);
+            break;
+        case RIGHT:
+            itsLevel->getItsSpawnerList().at(1)->appears(enemy);
+            break;
         }
+        itsLevel->appears(enemy);
+        itsEllapsedTime = 0;
+    }
 
     system("clear");
     checkAllCollid();
@@ -327,9 +327,9 @@ bool Game::collid(Entity * entity1, Entity * entity2){
 void Game::moveAll(){
     itsPlayer->move();
     if(!itsLevel->getItsEnemiesList().empty()){//à revoir cette condition est pas propre
-        for (Enemy * enemy : itsLevel->getItsEnemiesList()){
-            enemy->move();
-        }
+    for (Enemy * enemy : itsLevel->getItsEnemiesList()){
+        enemy->move();
+    }
     }
 }
 
