705b6f3b1d3a0f6282733cb8f3012d90d8d04f6e
erwan godelle <erwan.godelle@etudiant.univ-lr.fr>
2023-06-15 14:27:55 +0200

    dbscore test

--- CharcloBrosse/TestsCharclobrosse/TestDB_Score/TestDB_Score.pro
+++ CharcloBrosse/TestsCharclobrosse/TestDB_Score/TestDB_Score.pro
@@ -3,4 +3,8 @@ QT += gui widgets
 CONFIG += qt warn_on depend_includepath testcase
 TEMPLATE = app
 
-SOURCES +=  tst_test_db_score.cpp
+SOURCES +=  tst_test_db_score.cpp \
+    ../../CharcloBrosse/db_score.cpp
+
+HEADERS += \
+    ../../CharcloBrosse/db_score.h
--- CharcloBrosse/TestsCharclobrosse/TestDB_Score/tst_test_db_score.cpp
+++ CharcloBrosse/TestsCharclobrosse/TestDB_Score/tst_test_db_score.cpp
@@ -1,7 +1,7 @@
 #include <QtTest>
 #include <QCoreApplication>
+#include "../../CharcloBrosse/db_score.h"
 
-// add necessary includes here
 
 class test_DB_Score : public QObject
 {
@@ -12,36 +12,42 @@ public:
     ~test_DB_Score();
 
 private slots:
-    void initTestCase();
-    void cleanupTestCase();
-    void test_case1();
-
+    void test_saveScore();
+    void test_isInTop10();
 };
 
-test_DB_Score::test_DB_Score()
-{
+test_DB_Score::test_DB_Score() {}
 
+test_DB_Score::~test_DB_Score() {}
+
+void test_DB_Score::test_saveScore()
+{
+    DB_Score db;
+    unsigned int testScore = 100;
+
+    db.saveScore("TestPlayer", testScore);
+
+    // Vérifier si le score a bien été sauvegardé
+    std::vector<std::pair<std::string, unsigned int>> scores = db.loadScores();
+    QVERIFY(std::find_if(scores.begin(), scores.end(), [&testScore](const std::pair<std::string, unsigned int>& pair){
+        return pair.second == testScore;
+    }) != scores.end());
 }
 
-test_DB_Score::~test_DB_Score()
+void test_DB_Score::test_isInTop10()
 {
+    DB_Score db;
+    unsigned int testScore = 200;
 
+    db.saveScore("TestPlayer", testScore);
+
+    // Vérifier si le score est dans le top 10
+    QVERIFY(db.isInTop10(testScore));
+
+    // Vérifier si un score plus bas n'est pas dans le top 10
+    QVERIFY(!db.isInTop10(testScore - 1));
 }
 
-void test_DB_Score::initTestCase()
-{
-
-}
-
-void test_DB_Score::cleanupTestCase()
-{
-
-}
-
-void test_DB_Score::test_case1()
-{
-
-}
 
 QTEST_MAIN(test_DB_Score)
 
